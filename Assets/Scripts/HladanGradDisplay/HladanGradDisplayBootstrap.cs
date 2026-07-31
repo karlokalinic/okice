96cfeaa29d86acef36e2983020dc6790c3de296d
KARLOLEGEND <kalini.karlo@gmail.com>
2026-07-31 07:03:00 +0200

    Minimize display bootstrap

--- Assets/Scripts/HladanGradDisplay/HladanGradDisplayBootstrap.cs
+++ Assets/Scripts/HladanGradDisplay/HladanGradDisplayBootstrap.cs
@@ -2,16 +2,11 @@ using UnityEngine;
 
 public static class HladanGradDisplayBootstrap
 {
-    private const int ProductionFrameRate = 60;
-
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     private static void Init()
     {
-        // Do not bind simulation/render cost to the physical monitor refresh rate. On 144/165/200 Hz
-        // displays the previous code forced the restored project and all fullscreen effects to run
-        // several times more often than the original production target.
         QualitySettings.vSyncCount = 0;
-        Application.targetFrameRate = ProductionFrameRate;
+        Application.targetFrameRate = 60;
         Application.backgroundLoadingPriority = ThreadPriority.Normal;
     }
 }
